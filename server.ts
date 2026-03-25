6bf67598cfd813f4c040b8c10ed02e22e59a124d
Owner <owner@Owners-iMac.local>
2026-03-25 13:12:01 -0100

    add webhook verification route

--- server.ts
+++ server.ts
@@ -77,3 +77,16 @@ async function startServer() {
 }
 
 startServer();
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = "aventour_token_123";
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
